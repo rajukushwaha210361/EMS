cc60a5e2692b61a9f1eca2fe997954ca146f2e50
Rajukushwaha210361 <rajukushwaha210361@gmail.com>
2025-04-13 12:16:54 +0530

    Fixed missing Bootstrap dependency

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // import AuthContext from './Context/AuthContext';
 // import TaskContext from './Context/AuthContext';
 import AuthProvider from './Context/AuthProvider';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 // localStorage.clear()
 ReactDOM.createRoot(document.getElementById('root')).render(
